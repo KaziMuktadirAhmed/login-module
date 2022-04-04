e7126345b9e17ef6ce61f2290d9e78e3a813f580
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-04-05 02:14:38 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+
+    $_SESSION;
+
+?>
+
+<html>
+    <head>
+        <title>Authntication</title>
+    </head>
+
+    <body>
+        <h1>index.php</h1>
+        <a href="logout.php">logout</a>
+        
+        <br>
+        Hello, friends this is ............ speaking 
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+
+    <body>
+        <div id="box">
+            <form method="POST">
+
+                <input type="email" name="email" required>
+                <input type="password" name="password" required>
+
+                <input type="submit" value="Login">
+
+                <a href="signup.php">Sign up</a>
+            </form>
+        </div>
+
+    </body>
+</html>
